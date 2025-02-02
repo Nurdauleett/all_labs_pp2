38bc7ad4e291b3dc189d2cd56750e9f3cb77a17a
Nurdaulet <zarlykan.nurdaulet@gmail.com>
2025-02-02 20:14:10 +0500

    upd

---
+++ lab2/dictionaries.py
@@ -0,0 +1,53 @@
+#1
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+#2
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(thisdict)
+
+#3
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(thisdict["brand"])
+
+#4
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964,
+  "year": 2020
+}
+print(thisdict)
+
+#5
+print(len(thisdict))
+
+#6
+thisdict = {
+  "brand": "Ford",
+  "electric": False,
+  "year": 1964,
+  "colors": ["red", "white", "blue"]
+}
+
+#7
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(type(thisdict))
+
+#8
+thisdict = dict(name = "John", age = 36, country = "Norway")
+print(thisdict)
